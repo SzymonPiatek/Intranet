cdd68bec2105cb0955509b83e87eca5fe2e2cd2e
Szymon Piątek <szymon.piatek125@gmail.com>
2024-03-25 08:23:47 +0100

    Home Page SCSS

--- website/website/views.py
+++ website/website/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from parkings.models import ParkingSpot
 
 
 @login_required
 def home_view(request):
-    return render(request, "home.html")
+    user = request.user
+    user_have_parking_spot = ParkingSpot.objects.filter(owners=user).exists()
+
+    context = {
+        "user_have_parking_spot": user_have_parking_spot,
+    }
+
+    return render(request, "home.html", context)
