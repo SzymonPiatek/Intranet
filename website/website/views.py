60600d8304e683b32a0d28c623915ca6bf1ecadd
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-04-08 14:58:03 +0200

    Application popup overlay and hover effect (#12)
    * Hover
    
    * Calendar code improvement and hover scss
    
    * Overlay display
    
    * Display overlay
    
    * Basic Form
    
    * Basic SCSS
    
    ---------
    
    Co-authored-by: Szymon Piątek <szymon.piatek@pisf.pl>

--- website/website/views.py
+++ website/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from parkings.models import ParkingSpot
+from applications.models import Status
 
 
 @login_required
@@ -8,8 +9,11 @@ def home_view(request):
     user = request.user
     user_have_parking_spot = ParkingSpot.objects.filter(owner=user).exists()
 
+    application_statuses = Status.objects.all()
+
     context = {
         "user_have_parking_spot": user_have_parking_spot,
+        "application_statuses": application_statuses,
     }
 
     return render(request, "home.html", context)
