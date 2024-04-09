67d63876f4d33af47dcf2d1ec31e87918281eaf9
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-09 09:13:23 +0200

    Drop to dropbox

--- website/static/js/home/drop_box.js
+++ website/static/js/home/drop_box.js
@@ -6,18 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
         fileInput.click();
     });
 
-    fileInput.addEventListener('change', function() {
-    const allowedTypes = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const file = fileInput.files[0];
-
-    if (!allowedTypes.includes(file.type)) {
-        alert('Only PDF, DOC, and DOCX files are allowed.');
-        fileInput.value = '';
-        return false;
-    }
-
-    const fileName = file.name;
-    const dropBox = document.getElementById('dropBox');
-    dropBox.querySelector('.file_name').textContent = fileName;
+    dropBox.addEventListener('dragover', function(e) {
+        e.preventDefault();
+        dropBox.classList.add('dragover');
     });
+
+    dropBox.addEventListener('dragleave', function() {
+        dropBox.classList.remove('dragover');
+    });
+
+    dropBox.addEventListener('drop', function(e) {
+        e.preventDefault();
+        dropBox.classList.remove('dragover');
+        const file = e.dataTransfer.files[0];
+        handleFile(file);
+    });
+
+    fileInput.addEventListener('change', function() {
+        const file = fileInput.files[0];
+        handleFile(file);
+    });
+
+    function handleFile(file) {
+        const allowedTypes = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
+
+        if (!allowedTypes.includes(file.type)) {
+            alert('Only PDF, DOC, and DOCX files are allowed.');
+            fileInput.value = '';
+            return false;
+        }
+
+        const fileName = file.name;
+        const dropBox = document.getElementById('dropBox');
+        dropBox.querySelector('.file_name').textContent = fileName;
+    }
 });
