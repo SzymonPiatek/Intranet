215521a9bb03f04f6c0ee3780ac77f7433594a2d
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-09 09:06:49 +0200

    Overlay display

--- website/static/js/home/overlay.js
+++ website/static/js/home/overlay.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const blocks = document.querySelectorAll('.overlay .container .content .block');
     const overlay = document.getElementById('overlay');
     const createApplicationButton = document.getElementById('createApplicationButton');
     const closeOverlayButton = document.getElementById('closeOverlay');
+    const createApplication = document.getElementById('createApplication');
+
+    function changeBlockDisplay(block) {
+        if (block.style.display === 'none') {
+            block.style.display = 'flex';
+        } else {
+            block.style.display = 'none';
+        }
+    }
 
     function removeOverlayDisplay() {
         if (overlay.classList.contains('show')) {
             overlay.classList.remove('show');
+            blocks.forEach(blockDiv => {
+                blockDiv.style.display = 'none';
+            });
         }
         event.stopPropagation();
     }
@@ -18,10 +31,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     createApplicationButton.addEventListener('click', function() {
-        addOverlayDisplay()
+        changeBlockDisplay(createApplication);
+        addOverlayDisplay();
     });
 
     closeOverlayButton.addEventListener('click', function() {
-        removeOverlayDisplay()
+        removeOverlayDisplay();
     })
 });
